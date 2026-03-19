6a0fde84eca1af30977b8fab901963881cd8bbd8
gabrielfifa777-cmyk <gabrielfifa777@gmail.com>
2026-03-19 11:42:27 -0300

    aula 02 variáveis

---
+++ aula02a-variáveis.py
@@ -0,0 +1,34 @@
+print ("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print ("7" + "4")
+
+# CONCATEANDO STRINGS
+'''
+Comentários de
+multiplas 
+linhas
+'''
+
+#VARIÁVEIS
+nome = "Gabriel" # str
+idade = 26 # int
+peso = 59 #float
+
+print("oiii \n", nome, idade, peso)
+print (f"Olá, {nome} !!!")
+
+# INPUT -- SIMULAÇÃO DE UM FORMS DE CMD
+nome = input ("Digite o seu nome:")
+idade = int(input("Digite sua idade"))
+peso = float(input("Digites eu peso: "))
+print (nome, idade, peso)
+print (idade + 1)
+
+
+ano_nascimento = 2007
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print (f"Sua idade é: {idade}")
+
